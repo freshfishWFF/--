c275f9813b79d0f90760bd47b1016c51f1cb0320
dxy <1006196528@qq.com>
2021-02-03 13:57:20 +0800

    vue2.0template

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <div style="position: relative; display: inline-block">
+      <canvas id="mycanvas" width="150" height="150"></canvas>
+      <input
+        type="text"
+        name=""
+        id="inputs"
+        value="12345"
+        style="
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%, -50%);
+          width: 30px;
+          display: none;
+          font-size: 15px;
+          text-align: center;
+        "
+      />
+    </div>
+    <!-- <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul> -->
+  </div>
+</template>
+
+<script>
+// import CircleProgress from "../assets/canvasPro";
+import CircleProgress from "../assets/canvasNew";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      objs: null,
+      num: 0,
+    };
+  },
+  mounted() {
+    this.objs = new CircleProgress(
+      "mycanvas",
+      "inputs",
+      "#ddd",
+      "#0f5478",
+      "#000",
+      60,
+      50,
+      '℉'
+    );
+    // setInterval(()=>{console.log(this.objs.num)},1000)
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
